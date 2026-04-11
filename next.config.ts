62c74c5f3cc88335a86d435ce6c28ae7528ab18c
devAdilet <devzhusupov@gmail.com>
2026-04-10 23:56:45 -0700

    ci: add GitHub Pages deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = "Pizza";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isGithubActions ? `/${repo}` : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
